9766d65a32de6b0c0179cf3f149b3295e21694ce
abmm <abian83@gmail.com>
2016-05-05 12:35:02 +0200

    Disable ActiveRecord

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
-
+#require 'rails/all'
+#Disable ActiveRecord
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -21,6 +25,6 @@ module DemoBbva
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    #config.active_record.raise_in_transactional_callbacks = true
   end
 end
