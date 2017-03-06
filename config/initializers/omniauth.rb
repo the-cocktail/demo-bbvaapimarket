3311e59d299011fb31ed1b38f04a301ffec2da00
Enol Iglesias <enoliglesias@gmail.com>
2017-03-06 15:41:54 +0100

    Use settings in omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,12 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  CLIENT_ID     = ENV["BBVA_CLIENT_ID"]
-  CLIENT_SECRET = ENV["BBVA_CLIENT_SECRET"]
+  CLIENT_ID     = Settings.bbva.client_id
+  CLIENT_SECRET = Settings.bbva.client_secret
   #if some ENV variables are not defined Omniauth::Bbva will take the default sandbox ones
   args = {
-    site:           ENV['BBVA_SITE'], 
-    authorize_url:  ENV['BBVA_AUTHORIZE_URL'],
-    token_url:      ENV['BBVA_TOKEN_URL'],
-    callback:       ENV['BBVA_CALLBACK']# by default => http://localhost:3000/auth/bbva/callback
+    site:           Settings.bbva.site,
+    authorize_url:  Settings.bbva.authorize_url,
+    token_url:      Settings.bbva.token_url,
+    callback:       Settings.bbva.callback # by default => http://localhost:3000/auth/bbva/callback
   }
   provider :bbva , CLIENT_ID, CLIENT_SECRET, args
 end
