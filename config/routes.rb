46b430fe757685d85e0d53645674da5756dc5d0e
Enol Iglesias <enoliglesias@gmail.com>
2017-03-07 12:53:37 +0100

    Add neede routes for funnel func

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :accounts
   resources :cards
   resources :me, only: [:index]
+  resources :funnel, only: [:index]
+  get 'funnel/buy', to: 'funnel#buy'
   get '/me/samples', :to => 'me#samples'
   #BBVA Omniauth callback
   get '/auth/:bbva/callback', :to => 'sessions#create'
