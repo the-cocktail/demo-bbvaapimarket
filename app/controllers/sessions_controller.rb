a33ecf2388a6899136083d862d64228f7508280e
Enol Iglesias <enoliglesias@gmail.com>
2017-03-07 12:55:34 +0100

    Add redirection to buy view when access to products in public view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ActionController::Base
 
   def create
     session[:auth] = request.env['omniauth.auth']
-    redirect_to accounts_path
+    redirect_to session[:last_url].present? ? session[:last_url] : accounts_path
   end
-  
+
   def destroy
     session[:auth] = nil
     redirect_to :root
