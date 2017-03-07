98a33b8aa570193d4e5bac1473c4945917953ec6
Enol Iglesias <enoliglesias@gmail.com>
2017-03-07 16:10:12 +0100

    Add 3 products in funnel

--- app/controllers/funnel_controller.rb
+++ app/controllers/funnel_controller.rb
@@ -11,19 +11,19 @@ class FunnelController < ApplicationController
     if @client.accounts.any? && @client.accounts.map{|account| account["balance"]}.max > 8_000
       @text = I18n.t(".more_than_8000")
       @partial = "accounts"
-      @price = 180
+      @prices = [180, 680, 150]
     elsif @client.accounts.size > 2
       @text = I18n.t(".more_than_2_accounts")
       @partial = "accounts"
-      @price = 220
+      @prices = [220, 750, 200]
     elsif @client.cards.size >= 2
       @text = I18n.t(".more_or_equal_than_2_cards")
       @partial = "cards"
-      @price = 200
+      @prices = [200, 700, 180]
     else
       @text = I18n.t(".default_text")
       @partial = "default"
-      @price = 250
+      @prices = [250, 900, 250]
     end
   end
 
