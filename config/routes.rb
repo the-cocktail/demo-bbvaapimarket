3ca444feedd7504f24f09f7712d2e5a2289cf722
Enol Iglesias <enoliglesias@gmail.com>
2017-03-13 10:29:46 +0100

    Add routes for payment and transfer

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :funnel, only: [:index]
   get 'funnel/buy_bbva', to: 'funnel#buy_bbva'
   get 'funnel/buy', to: 'funnel#buy'
+  get 'funnel/payment/:id', to: 'funnel#payment', as: :payment
+  post 'funnel/transfer/:id', to: 'funnel#transfer', as: :transfer
   get '/me/samples', :to => 'me#samples'
   #BBVA Omniauth callback
   get '/auth/:bbva/callback', :to => 'sessions#create'
