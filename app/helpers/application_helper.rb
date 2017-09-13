97b6567b9dcc3952c3e229b17641186c3d339ae5
Daniel Romero <daniel.romero.esteban@gmail.com>
2017-09-13 13:14:20 +0200

    fix api changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-  
+
   def client_full_name
-    "#{@client[:name].humanize} #{@client[:surname].humanize} #{@client[:secondSurname].humanize}" if @client
+    "#{@client[:firstName].humanize} #{@client[:surname].humanize} #{@client[:secondSurname].humanize}" if @client
   end
 
   #[01: Cuenta corriente, 02: Cuenta crédito]
   def account_type type
     case type
-      when "01" 
+      when "01"
         "Current Account"
       when "02"
         "Credit Account"
@@ -19,7 +19,7 @@ module ApplicationHelper
   #[01: Debit Card, 02: Credit Card, 03: Prepaid Card]
   def card_type type
     case type
-      when "01" 
+      when "01"
         "Debit Card"
       when "02"
         "Credit Card"
